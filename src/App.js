3d1c50fa4469a0cace38f911e02e0ea8bf16fd90
labCap <banlazthosow@gmail.com>
2022-12-04 15:56:56 +0200

    html +

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <div className="todo">
+        <div className="container">
+          <div className="todo__inner">
+            <div className="todo__form">
+              <input type="text" />
+              <button>
+                <div></div>
+                <div></div>
+              </button>
+            </div>
+            <div className="todo__list">
+              <div className="todo__item">
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Deleniti repellendus accusamus nulla exercitationem, iusto
+                  facilis porro! Temporibus assumenda veniam ea accusamus sed,
+                  ipsa labore rerum, omnis, qui dolore reiciendis ullam.
+                </p>
+                <i class="fa-solid fa-trash"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
